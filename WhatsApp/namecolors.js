9e18cab1f02df123d6583e0725caa7628d0f0b67
Molly Taylor <42254557+motaylormo@users.noreply.github.com>
2020-05-02 13:37:48 -0700

    Update namecolors.js

--- WhatsApp/namecolors.js
+++ WhatsApp/namecolors.js
@@ -1,45 +1,50 @@
 function selectColor(colorNum, colors)
 {
-    if (colors < 1) colors = 1; // defaults to one color - avoid divide by zero
-    return "hsl(" + (colorNum * (360 / colors) % 360) + ",90%,45%)";
+	if (colors < 1) colors = 1; // defaults to one color - avoid divide by zero
+	return "hsl(" + (colorNum * (360 / colors) % 360) + ",90%,45%)";
 }
 
 function getColorName(name, i, len)
 {
-  let newLine = document.createElement("span");
-  newLine.textContent = name;
-  newLine.style.color = selectColor(i, len);
-  newLine.style.fontWeight = "bold";
-  newLine.style.display = "block";
-  
-  newLine.style.filter = "hue-rotate(-30deg)";
-  return (newLine);
+	let newLine = document.createElement("span");
+	newLine.textContent = name;
+	newLine.style.color = selectColor(i, len);
+	newLine.style.fontWeight = "bold";
+	newLine.style.display = "block";
+	newLine.style.filter = "hue-rotate(-30deg)";
+	return (newLine);
 }
 
-var items = new Object();
-var outgoing_sections = document.querySelectorAll(".in[name]");
-
-//get all names
-for (let i = 0; i < outgoing_sections.length; i++) {
-  let name = outgoing_sections[i].getAttributeNode("name").value;
-  if (!items[name]) {
-    items[name] = undefined;
-  }
+function extractNames(outgoing_sections)
+{
+	var items = new Object();
+	outgoing_sections.forEach(function(element){
+		let name = element.getAttributeNode("name").value;
+		if (!items[name]) {
+			items[name] = undefined;
+		}
+	});
+	return (items);
 }
 
-// names to colors
-var len = Object.keys(items).length;
-var i = 0;
-
-Object.keys(items).forEach(function(k){
-      items[k] = getColorName(k, i++, len);
-});
-
-console.log(items);
-
-//set names
-for (let i = 0; i < outgoing_sections.length; i++) {
-  let name = outgoing_sections[i].getAttributeNode("name").value;
-  let where = outgoing_sections[i].querySelector(":scope dd:first-of-type");
-  where.prepend(items[name].cloneNode(true));
+function setNames(outgoing_sections, items)
+{
+	outgoing_sections.forEach(function(element){
+		let name = element.getAttributeNode("name").value;
+		let where = element.querySelector(":scope dd:first-of-type");
+		where.prepend(items[name].cloneNode(true));
+	});
 }
+
+document.addEventListener("DOMContentLoaded", function(){
+	var outgoing_sections = document.querySelectorAll(".whatsapp.grouptext .in[name]");
+	var items = extractNames(outgoing_sections);
+
+	// names to colors
+	var len = Object.keys(items).length;
+	Object.keys(items).forEach(function(k, index){
+				items[k] = getColorName(k, index, len);
+	});
+	
+	setNames(outgoing_sections, items);
+})
